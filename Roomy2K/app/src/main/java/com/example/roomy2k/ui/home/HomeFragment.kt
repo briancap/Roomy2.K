91a9d4cba0e4576d08fa6d46dc45cef1f5d0dfa7
Brian Cap <briancap@pop-os.localdomain>
2020-09-14 22:28:56 -0400

    add custom onItemClick for RecyclerView

--- Roomy2K/app/src/main/java/com/example/roomy2k/ui/home/HomeFragment.kt
+++ Roomy2K/app/src/main/java/com/example/roomy2k/ui/home/HomeFragment.kt
@@ -1,12 +1,11 @@
 package com.example.roomy2k.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -14,34 +13,51 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.roomy2k.R
 import com.example.roomy2k.TestData
 import com.example.roomy2k.adapters.AdapterGridFab
-import com.example.roomy2k.adapters.AdapterGridItem
 import com.example.roomy2k.adapters.AdapterSharedItems
+import com.example.roomy2k.custom.RecyclerItemClickListener
+
 
 class HomeFragment : Fragment() {
 
+    val LOG_TAG: String = javaClass.name
     private lateinit var homeViewModel: HomeViewModel
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
 
         //Bill Section
-        val gridBills: RecyclerView = root.findViewById( R.id.home_grid_bills )
-        gridBills.setHasFixedSize( true )
-        gridBills.layoutManager = GridLayoutManager( context, 4 )
+        val gridBills: RecyclerView = root.findViewById(R.id.home_grid_bills)
+        gridBills.setHasFixedSize(true)
+        gridBills.layoutManager = GridLayoutManager(context, 4)
         gridBills.adapter = AdapterGridFab(context, TestData(context).getBills())
 
         //Shared Item Section
-        val listSharedItems: RecyclerView = root.findViewById( R.id.home_list_shared )
-        listSharedItems.setHasFixedSize( true )
-        listSharedItems.layoutManager = LinearLayoutManager( context )
-        listSharedItems.adapter = AdapterSharedItems( context, TestData( context ).getSharedItems() )
+        val listSharedItems: RecyclerView = root.findViewById(R.id.home_list_shared)
+        listSharedItems.setHasFixedSize(true)
+        listSharedItems.layoutManager = LinearLayoutManager(context)
+        listSharedItems.adapter = AdapterSharedItems(context, TestData(context).getSharedItems())
 
+        listSharedItems.addOnItemTouchListener(
+            RecyclerItemClickListener(
+                context,
+                listSharedItems,
+                object : RecyclerItemClickListener.OnItemClickListener {
+                    override fun onItemClick(view: View?, position: Int) {
+                        Log.e( LOG_TAG, "onItemClick" )
+                    }
+
+                    override fun onLongItemClick(view: View?, position: Int) {
+                        Log.e( LOG_TAG, "onLongItemClick" )
+                    }
+                }
+            )
+        )
 /*
         //Chores Section
         val gridChores: RecyclerView = root.findViewById( R.id.home_grid_chores )
