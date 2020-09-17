74fa054c7165e5d8c8d041d8cfbe5f6628e2f052
Brian Cap <briancap@pop-os.localdomain>
2020-09-16 22:03:37 -0400

    add conditional intent data passing

--- Roomy2K/app/src/main/java/com/example/roomy2k/HomeDetailActivity.kt
+++ Roomy2K/app/src/main/java/com/example/roomy2k/HomeDetailActivity.kt
@@ -1,15 +1,19 @@
 package com.example.roomy2k
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 
-class BillDetailActivity : AppCompatActivity() {
+class HomeDetailActivity : AppCompatActivity() {
 
+    val LOG_TAG: String = javaClass.name
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.placeholder)
+
+    
     }
 
 }
--- Roomy2K/app/src/main/java/com/example/roomy2k/ui/home/HomeFragment.kt
+++ Roomy2K/app/src/main/java/com/example/roomy2k/ui/home/HomeFragment.kt
@@ -2,18 +2,16 @@ package com.example.roomy2k.ui.home
 
 import android.content.Intent
 import android.os.Bundle
-import android.provider.AlarmClock.EXTRA_MESSAGE
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.roomy2k.BillDetailActivity
+import com.example.roomy2k.HomeDetailActivity
 import com.example.roomy2k.R
 import com.example.roomy2k.TestData
 import com.example.roomy2k.adapters.AdapterGridFab
@@ -41,6 +39,28 @@ class HomeFragment : Fragment() {
         gridBills.layoutManager = GridLayoutManager(context, 4)
         gridBills.adapter = AdapterGridFab(context, TestData(context).getBills())
 
+        gridBills.addOnItemTouchListener(
+            RecyclerItemClickListener(
+                context,
+                gridBills,
+                object : RecyclerItemClickListener.OnItemClickListener{
+                    override fun onItemClick(view: View?, position: Int) {
+                        Log.e(LOG_TAG, "gridBills; onItemClick")
+                        val intent = Intent( context, HomeDetailActivity::class.java ).apply {
+                            putExtra( resources.getString( R.string.intent_activity_start_reason ), resources.getString(R.string.intent_activity_start_reason_detail_bill) )
+                        }
+                        startActivity( intent )
+                    }
+
+                    override fun onLongItemClick(view: View?, position: Int) {
+                        Log.e(LOG_TAG, "gridBills; onLongItemClick")
+                        onItemClick( view, position )
+                    }
+                }
+            )
+        )
+
+
         //Shared Item Section
         val listSharedItems: RecyclerView = root.findViewById(R.id.home_list_shared)
         listSharedItems.setHasFixedSize(true)
@@ -53,34 +73,21 @@ class HomeFragment : Fragment() {
                 listSharedItems,
                 object : RecyclerItemClickListener.OnItemClickListener {
                     override fun onItemClick(view: View?, position: Int) {
-                        Log.e(LOG_TAG, "onItemClick")
-                        val intent = Intent( context, BillDetailActivity::class.java ).apply {
-                            putExtra( "test", "test value")
+                        Log.e(LOG_TAG, "listSharedItems; onItemClick")
+                        val intent = Intent( context, HomeDetailActivity::class.java ).apply {
+                            putExtra( resources.getString( R.string.intent_activity_start_reason ), resources.getString(R.string.intent_activity_start_reason_detail_shared) )
                         }
                         startActivity( intent )
                     }
 
                     override fun onLongItemClick(view: View?, position: Int) {
-                        Log.e(LOG_TAG, "onLongItemClick")
+                        Log.e(LOG_TAG, "listSharedItems; onLongItemClick")
+                        onItemClick( view, position )
                     }
                 }
             )
         )
-/*
-        //Chores Section
-        val gridChores: RecyclerView = root.findViewById( R.id.home_grid_chores )
-        gridChores.setHasFixedSize( true )
-        gridChores.layoutManager = GridLayoutManager( context, 4 )
-        gridChores.adapter = AdapterGridItem( context, TestData( context ).getChores() )
-*/
 
-        /*
-        val textView: TextView = root.findViewById(R.id.home_grid_bills)
-
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-         */
         return root
     }
 }
